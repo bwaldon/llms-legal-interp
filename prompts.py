df75a8bf4fc042ee6bdb7855752e7e2422a4161f
Abhishek Purushothama <abhishek.purushothama@colorado.edu>
2025-03-19 00:05:01 -0400

    Yes or No Prompts

--- prompts.py
+++ prompts.py
@@ -1,37 +1,50 @@
-import dspy
 from typing import Literal
+import re
+
+from datasets import Dataset, load_dataset
+
+
+def get_prompt_dataset(prompt_function, prompt_type):
+    def convert_header_to_definition(header):
+        match re.findall(r"<b>\"(.*?)\"</b>", header):
+            case [x]:
+                return x
+            case _:
+                return None
+
+    VAGUE_CONTRACTS_FILE = "data/vague_contracts.csv"
+    vague_contracts_dataset = load_dataset("csv", data_files={"test": VAGUE_CONTRACTS_FILE})
+    vague_contracts_dataset["test"] = vague_contracts_dataset["test"].map(
+        lambda x: {"definition": convert_header_to_definition(x["header"])}, batched=False)
+    print(vague_contracts_dataset.shape)
+    vague_contracts_prompts = vague_contracts_dataset.map(lambda x: {
+        "prompt": prompt_function(x),
+        "prompt_type": prompt_type,
+    })
+    return vague_contracts_prompts
+
 #1 Binary (Meaning) Judgements
 # Question style similar to summarization from Blair-Stanek et al.
 def binary_option_question(scenario, definition, options):
-    return f"""
-        Consider the following:
+    return f"""Consider the following:
         {scenario} 
         Does it contain an instance of {definition}? Answer with ONLY the string {options[0]} or {options[1]}:   
         """
-def no_or_yes_question(scenario, definition):
-    return binary_option_question(scenario, definition, ['No', 'Yes'])
 
-def yes_or_no_question(scenario, definition):
-    return binary_option_question(scenario, definition, ['Yes', 'No'])
+def no_or_yes_question(item):
+    return binary_option_question(item["scenario"], item["definition"], ['No', 'Yes'])
 
-def make_it_do_you_agree(item, actor = "Insurance Company"):
+
+def yes_or_no_question(item):
+    return binary_option_question(item["scenario"], item["definition"], ['Yes', 'No'])
+
+
+def make_it_do_you_agree(item, actor="Insurance Company"):
     return f"""{item.header}
     {item.continuation}
     {actor} decided that the claim is {item.version} under the policy.
     Do you agree?
     """
 
-
-
-class YesOrNoPrompt(dspy.Signature):
-    """Make it a classification Yes, or No question."""
-
-    question: str = dspy.InputField()
-    sentiment: Literal['Yes', 'No'] = dspy.OutputField()
-
-if __name__ == '__main__':
-    print(yes_or_no_question(
-        "Gavin works as an A/C repair technician in a small town. One day, Gavin is hired to repair an air conditioner located on the second story of a building. Because Gavin is an experienced repairman, he knows that the safest way to access the unit is with a sturdy ladder. While climbing the ladder, Gavin loses his balance and falls, causing significant injury. Because of this, he subsequently has to stop working for weeks. Gavin files a claim with his insurance company for lost income.",
-        "missed employment due to injuries that occur under regular working conditions."
-    ))
-
+def get_yes_or_no_vague_contracts():
+    return get_prompt_dataset(yes_or_no_question, "yes_or_no")
