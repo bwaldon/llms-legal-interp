b3a0fd78eb73a0c438666ce7c0d0d53f567ae722
Aatlantise <jmin10@jhu.edu>
2025-03-26 00:49:38 -0400

    [Major feature] Implement model and data class
    * Add MetaLinguisticPrompt class with topic and nudge ablation
    * Add MetaLinguisticJudgement class for easy inference
    * Add pipeline script

--- main.py
+++ main.py
@@ -1,70 +1,50 @@
-from vllm import LLM, SamplingParams
+from model import MetaLinguisticPrompt, MetaLinguisticJudgement
+from huggingface_hub import login
 import csv
 import gc
 import torch
 
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-
-# llm = LLM(model="facebook/opt-125m")
 model_list = [
-    # "meta-llama/Llama-3.2-1B",
+    "meta-llama/Llama-3.2-1B-Instruct",
+    "meta-llama/Llama-3.2-1B",
     # "meta-llama/Llama-3.2-3B",
+    # "meta-llama/Llama-3.2-3B-Instruct",
     # "meta-llama/Llama-3.2-8B",
     # "gpt2-medium",
     # "gpt2-large",
     # "gpt2-xl",
-    "bigscience/bloom-560m"
-    "bigscience/bloom-1b1",
-    "bigscience/bloom-3b",
-    "bigscience/bloom-7b1"
+    # "bigscience/bloom-560m"
+    # "bigscience/bloom-1b1",
+    # "bigscience/bloom-3b",
+    # "bigscience/bloom-7b1"
 ]
 
+login(token=open("hf_token.txt").read())
 
-def get_relevant_fields(row):
-    return row[3], row[4], row[11]
+for model_name in model_list:
 
-with open("vague_contracts/data/1_falseconsensus/demo-merged.csv") as f:
-    r = csv.reader(f, delimiter=",", quotechar='"')
-    data = [row for row in r]
-
-context, policy, ambiguity = get_relevant_fields(data[1])
-
-questions = [
-    "Should the insurer have accepted the claim? Please answer with either yes or no.",
-    "Shouldn't the insurer have accepted the claim? Please answer with either yes or no.",
-    "Should the insurer have accepted the claim? Please answer with either no or yes.",
-    "Shouldn't the insurer have accepted the claim? Please answer with either no or yes.",
-    "Should the insurer not accept the claim? Please answer with either no or yes.",
-    "Should the insurer have denied the claim? Please answer with either yes or no.",
-    "Shouldn't the insurer have denied the claim? Please answer with either yes or no.",
-    "Should the insurer have denied the claim? Please answer with either no or yes.",
-    "Shouldn't the insurer have denied the claim? Please answer with either no or yes.",
-    "Should the insurer not deny the claim? Please answer with either no or yes.",
-    "Wasn't the insurer correct in their decision to accept the claim?"
-]
-
-for model in model_list:
-    print("**************************")
-    print(model)
-
-    llm = LLM(model=model)
-
+    model = MetaLinguisticJudgement(model_name)
     prompts = [
-        (f"Consider the context: {context}"
-         f""
-         f"Consider the insurance policy: {policy}"
-         f""
-         f"{question}")
-        for question in questions
+        MetaLinguisticPrompt(topic="landscaping", nudge="No"),
+        MetaLinguisticPrompt(topic="landscaping", nudge=None),
+        MetaLinguisticPrompt(topic="landscaping", nudge="Yes"),
+        MetaLinguisticPrompt(topic="restraining", nudge="No"),
+        MetaLinguisticPrompt(topic="restraining", nudge=None),
+        MetaLinguisticPrompt(topic="restraining", nudge="Yes"),
     ]
 
-    outputs = llm.generate(prompts, sampling_params)
+    outputs = model.infer(prompts)
 
-    for output in outputs:
-        prompt = output.prompt
-        generated_text = output.outputs[0].text
-        print(generated_text)
+    print(model_name + "\n")
 
-    del llm
+    for p, o in zip(prompts, outputs):
+        print(f"*********************{p.topic}********************{p.nudge}*************************\n")
+        print(p.text)
+        print("\n\n")
+        print(o)
+        print("\n\n\n")
+
+
+    del model
     torch.cuda.empty_cache()
     gc.collect()
