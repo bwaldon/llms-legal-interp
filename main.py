e348f98710ea924622c6c73bfb98bf6ecf6ac819
Abhishek Purushothama <abhishek.purushothama@colorado.edu>
2025-03-27 11:47:49 -0400

    [Chore] Some readability organization in main

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ model_list = [
     # "bigscience/bloom-7b1"
 ]
 
-login(token=open("hf_token.txt").read())
 
-for model_name in model_list:
+def cleanup():
+    torch.cuda.empty_cache()
+    gc.collect()
 
-    model = MetaLinguisticJudgement(model_name)
-    prompts = [
+
+def get_prompts():
+    return [
         MetaLinguisticPrompt(topic="landscaping", nudge="No"),
         MetaLinguisticPrompt(topic="landscaping", nudge=None),
         MetaLinguisticPrompt(topic="landscaping", nudge="Yes"),
@@ -33,18 +35,28 @@ for model_name in model_list:
         MetaLinguisticPrompt(topic="restraining", nudge="Yes"),
     ]
 
+
+def load_and_infer_with_model(model_name, prompts):
+    print(model_name + "\n")
+    model = MetaLinguisticJudgement(model_name)
     outputs = model.infer(prompts)
 
-    print(model_name + "\n")
-
-    for p, o in zip(prompts, outputs):
-        print(f"*********************{p.topic}********************{p.nudge}*************************\n")
+    def print_prompts_and_output(p, o):
+        star_20 = '*' * 20
+        print(f"{star_20}{p.topic}{star_20}{p.nudge}{star_20}", end="\n\n")
         print(p.text)
         print("\n\n")
         print(o)
         print("\n\n\n")
 
-
+    for p, o in zip(prompts, outputs):
+        print_prompts_and_output(p, o)
     del model
-    torch.cuda.empty_cache()
-    gc.collect()
+
+
+if __name__ == "__main__":
+    login(token=open("hf_token.txt").read())
+    prompts = get_prompts()
+    for model_name in model_list:
+        load_and_infer_with_model(model_name, prompts)
+        cleanup()
