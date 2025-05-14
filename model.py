aaf4a29efd2ed4d220f81af1a42d22c6c75183f6
Aatlantise <jmin10@jhu.edu>
2025-05-13 22:40:28 -0400

    [Minor feature] Enable inference on 8b models

--- model.py
+++ model.py
@@ -100,11 +100,19 @@ class MetaLinguisticPrompt:
         return ""
 
 class MetaLinguisticJudgement:
-    def __init__(self, model_name, seed, max_model_len=1024):
+    def __init__(self, model_name, seed, max_model_len=256):
         self.model_name = model_name
-        self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=1024, seed=seed, prompt_logprobs=10, logprobs=10)
+        self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=256, seed=seed, prompt_logprobs=10, logprobs=10)
         self.max_model_len = max_model_len
-        self.llm = LLM(model_name, max_model_len=max_model_len, seed=seed)
+        self.llm = LLM(
+            model_name,
+            max_model_len=max_model_len,
+            seed=seed,
+            dtype="float16",
+            gpu_memory_utilization=0.95,
+            max_num_seqs=8,
+            swap_space=8,
+        )
 
     def infer(self, prompts: List[MetaLinguisticPrompt]) -> List[CompletionOutput]:
         outputs = self.llm.generate(prompts, self.sampling_params)
