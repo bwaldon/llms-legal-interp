2141a69cddf4138dbc5047672c68ccd655ef5d46
Abhishek Purushothama <ap2089@georgetown.edu>
2025-05-14 17:19:10 -0400

    Remove logprobs for decoding

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from typing import List
 class MetaLinguisticJudgement:
     def __init__(self, model_name, seed, max_model_len=256):
         self.model_name = model_name
-        self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=256, seed=seed, prompt_logprobs=10, logprobs=10)
+        self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=256, seed=seed)
         self.max_model_len = max_model_len
         self.llm = LLM(
             model_name,
