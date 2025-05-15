7692c3012257eb776103d6dc5cdd5a61109639d1
Abhishek Purushothama <ap2089@georgetown.edu>
2025-05-15 16:59:34 -0400

    WIP: Get probs for specific tokens

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 
 import gc
 import string
+import token
 from collections import namedtuple
 import re
 import random
@@ -74,7 +75,7 @@ def get_prompts():
     """
 
 
-def load_and_infer_with_model(model_name, seed, dataset):
+def load_and_infer_with_model(model_name, seed, dataset, tokens_of_interest=("Yes", "No", "A", "B")):
     def model_cleanup():
         torch.cuda.empty_cache()
         gc.collect()
@@ -83,27 +84,57 @@ def load_and_infer_with_model(model_name, seed, dataset):
     print(model_name + "\n")
 
     model = MetaLinguisticJudgement(model_name, seed)
+    def get_token_ids_of_interest(tokens_of_interest, vocab):
+        token_ids = dict()
+        for token in tokens_of_interest:
+            token_ids[token] =  None #list()
+            if token in vocab:
+                token_ids[token] = (vocab[token])
+
+            # if token.lower() in vocab:
+            #     token_ids[token].append(vocab[token.lower()])
+        return token_ids
+
+    token_ids_of_interest = get_token_ids_of_interest(tokens_of_interest, model.tokenizer.get_vocab())
     prompts = dataset["prompt"]
     outputs = model.infer(prompts)
 
-    # https://stackoverflow.com/questions/43647186/tokenize-based-on-white-space-and-trailing-punctuation
-    def extract_first_token(text):
-        return [x.strip("\"'\.!") for x in re.split(r"([a-zA-z]+)?\s+", text) if x][0]
-
     # Collate data and outputs
     def collate_data_and_outputs(model_name, dataset, outputs):
+
+        # https://stackoverflow.com/questions/43647186/tokenize-based-on-white-space-and-trailing-punctuation
+        def extract_first_answer_token(text):
+            return [x.strip("\"'\.!") for x in re.split(r"([a-zA-z]+)?\s+", text) if x][0]
+
+        def collate_logprobs_for_tokens_of_interest(n, token_ids_of_interest, output):
+            seq_len = len(output.logprobs)
+            tokens_probs = list()
+            for token in token_ids_of_interest:
+                tokens_probs[token] = np.ndarray((seq_len, 1))
+
+            for position, logprobs in enumerate(output.logprobs)[:n]:
+                for token, token_id in token_ids_of_interest.items():
+                    tokens_probs[token][position] = logprobs[token_id].logprob if token_id in logprobs else 0
+            return tokens_probs
+
         print(f"Dataset : {len(dataset)} {len(outputs)}")
+        # Just get the probs for the output
         # TODO batched processing
+        results_token_probs = [collate_logprobs_for_tokens_of_interest(1, token_ids_of_interest, output) for output in outputs]
         results_dict = {
             "title": dataset["title"],
             "prompt_type": dataset["prompt_type"],
             "prompt": dataset["prompt"],
             "version": dataset["version"],
-            "output": [extract_first_token(output.text) for output in outputs],
+            "output": [extract_first_answer_token(output.text) for output in outputs],
             "output_text": [output.text for output in outputs],
             "cum_logprob": [output.cumulative_logprob for output in outputs]
         }
 
+        # results_dict["Yes_prob"] = [token_probs["Yes"] for token_probs in results_token_probs]
+        # results_dict["No_prob"] = [token_probs["No"] for token_probs in results_token_probs]
+        # results_dict["A_prob"] = [token_probs["A"] for token_probs in results_token_probs]
+        # results_dict["B_prob"] = [token_probs["B"] for token_probs in results_token_probs]
         return results_dict
 
     del model
