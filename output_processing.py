698d22db72d75dc094253e34773f8f400736fd4b
Abhishek Purushothama <ap2089@georgetown.edu>
2025-05-18 14:40:30 -0400

    First pass JS Dist and Entropy Analysis
    JSD, Reversal, gt reversal dist counts
    Reversal Counts
    
    Add analysis notebook with the new seed runs (comments some models with inf distance)
    
    Checkpoint of analysis notebook

--- output_processing.py
+++ output_processing.py
@@ -7,23 +7,23 @@ import scipy as sp
 def get_aff_unaff_columns(prompt_type):
     match prompt_type:
         case "yes_or_no" | "no_or_yes" | "agreement" | "disagreement_negation":
-            return "Yes_prob", "No_prob"
-        case "disagreement" | "agreement_negation":
-            return "No_prob", "Yes_prob"
+            return "Yes_probs", "No_probs"
+        case "disagreement" | "agreement_negation" | "negation":
+            return "No_probs", "Yes_probs"
         case "options":
-            return "A_prob", "B_prob"
+            return "A_probs", "B_probs"
         case "options_flipped":
-            return "B_prob", "A_prob"
+            return "B_probs", "A_probs"
         case _:
             raise ValueError()
 
 
 def organize_distribution(model_results):
-    model_results["Yes_prob"] = np.exp(model_results["Yes_prob"].values)
-    model_results["No_prob"] = np.exp(model_results["No_prob"].values)
-    model_results["A_prob"] = np.exp(model_results["A_prob"].values)
-    model_results["B_prob"] = np.exp(model_results["B_prob"].values)
-    model_results["Other_prob"] = 1 - model_results["Yes_prob"] - model_results["No_prob"]
+    model_results["Yes_probs"] = model_results["Yes_probs"].transform(np.exp)
+    model_results["No_probs"] = model_results["No_probs"].transform(np.exp)
+    model_results["A_probs"] = model_results["A_probs"].transform(np.exp)
+    model_results["B_probs"] = model_results["B_probs"].transform(np.exp)
+    model_results["Other_prob"] = 1 - model_results["Yes_probs"] - model_results["No_probs"]
 
     for group, indices in model_results.groupby("prompt_type").indices.items():
         aff_column, unaff_column = get_aff_unaff_columns(group)
@@ -40,20 +40,25 @@ def calculate_js_distance(model_results):
         "version": list(),
         "prompt_type": list(),
         "js_dist": list(),
+        "more_than_reversal": list()
     }
     # control and variation pairs
     for group, df in model_results.groupby(['title', 'version'], sort=False):
         control_mask = df.prompt_type == "yes_or_no"
         control_probs = df[["Aff_prob", "UnAff_prob", "Other_prob"]][control_mask]
+        reversal_probs = df[["UnAff_prob", "Aff_prob", "Other_prob"]][control_mask]
+        reversal_dists = np.array([sp.spatial.distance.jensenshannon(x, y) for x, y in zip(control_probs.values, reversal_probs.values)])
         for variation in prompt_types[1:]:
             variation_mask = df.prompt_type == variation
             variation_probs = df[["Aff_prob", "UnAff_prob", "Other_prob"]][variation_mask]
             # TODO numpy it
             js_dist = np.array([sp.spatial.distance.jensenshannon(x, y) for x, y in zip(control_probs.values, variation_probs.values)])
+            more_than_reversal = js_dist > reversal_dists
             divergences["title"].append(group[0])
             divergences["version"].append(group[1])
             divergences["prompt_type"].append(variation)
             divergences["js_dist"].append(js_dist.mean())
+            divergences["more_than_reversal"].append(more_than_reversal.mean())
     df = pd.DataFrame.from_dict(divergences)
     return df
 
@@ -76,7 +81,7 @@ def summarize_missing_probs(df):
     non_options_mask = (df["prompt_type"] != "options") & (df["prompt_type"] != "options_flipped")
     options_mask = (df["prompt_type"] == "options") | (df["prompt_type"] == "options_flipped")
     return pd.concat([
-        (df[["Yes_prob", "No_prob"]][non_options_mask] == 0).astype(int).sum(axis=0),
+        (df[["Yes_probs", "No_probs"]][non_options_mask] == 0).astype(int).sum(axis=0),
         (df[["A_prob", "B_prob"]][options_mask] == 0).astype(int).sum(axis=0)
     ])
 
