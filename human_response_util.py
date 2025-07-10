9e03e1e4a58cbef491d246ded8a5edd621cd87e9
Abhishek Purushothama <ap2089@georgetown.edu>
2025-07-10 12:19:37 -0400

    WIP: Prelim analysis

--- human_response_util.py
+++ human_response_util.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 
 if __name__ ==  "__main__":
-    human_responses = pd.read_csv("data/human/main-merged.csv")
-    print(human_responses.columns)
     yes_responses = np.sum(human_responses["individual_judgment"] == "yes")
     no_responses = np.sum(human_responses["individual_judgment"] == "no")
     print(f"Yes proportion : {yes_responses/ (yes_responses + no_responses) }")
